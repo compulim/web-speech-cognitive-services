77e9951dc2b6b90958c30fc18f861833f523215c
William Wong <compulim@hotmail.com>
2018-07-09 03:05:55 -0700

    Use BasicSay

--- packages/playground/src/SayPane.js
+++ packages/playground/src/SayPane.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Composer } from 'react-say';
 import SpeakAlong from './SpeakAlong';
 
 export default class SayPane extends React.Component {
@@ -42,14 +41,29 @@ export default class SayPane extends React.Component {
         </button>
         {
           state.started &&
-            <Composer
-              speechSynthesis={ props.speechSynthesis }
-              speechSynthesisUtterance={ props.speechSynthesisUtterance }
-            >
-              <SpeakAlong text="Pooh is very social." voice={ props.voice } volume={ 0.1 } />
-              <SpeakAlong pitch={ 1.5 } text="After Christopher Robin, his closest friend is Piglet." voice={ props.voice } />
-              <SpeakAlong rate={ 1.5 } text="And he must often chooses to spend his time with one or both of them." voice={ props.voice } />
-            </Composer>
+            <React.Fragment>
+              <SpeakAlong
+                speak="Pooh is very social."
+                speechSynthesis={ props.speechSynthesis }
+                speechSynthesisUtterance={ props.speechSynthesisUtterance }
+                voice={ props.voice }
+                volume={ 0.1 }
+              />
+              <SpeakAlong
+                pitch={ 1.5 }
+                speak="After Christopher Robin, his closest friend is Piglet."
+                speechSynthesis={ props.speechSynthesis }
+                speechSynthesisUtterance={ props.speechSynthesisUtterance }
+                voice={ props.voice }
+              />
+              <SpeakAlong
+                rate={ 1.5 }
+                speak="And he must often chooses to spend his time with one or both of them."
+                speechSynthesis={ props.speechSynthesis }
+                speechSynthesisUtterance={ props.speechSynthesisUtterance }
+                voice={ props.voice }
+              />
+            </React.Fragment>
         }
       </div>
     );
