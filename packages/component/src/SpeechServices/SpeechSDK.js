e8a00cfd4f0167d58ac0881ba74c445d2c8c1a06
William Wong <compulim@hotmail.com>
2019-07-25 12:33:38 -0700

    Use ES5 build

--- packages/component/src/SpeechServices/SpeechSDK.js
+++ packages/component/src/SpeechServices/SpeechSDK.js
@@ -1,3 +1,3 @@
-import * as CognitiveServicesSpeechSDK from 'microsoft-cognitiveservices-speech-sdk/distrib/es2015/microsoft.cognitiveservices.speech.sdk';
+import * as CognitiveServicesSpeechSDK from 'microsoft-cognitiveservices-speech-sdk/distrib/lib/microsoft.cognitiveservices.speech.sdk';
 
 export default CognitiveServicesSpeechSDK
