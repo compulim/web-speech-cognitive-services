52761d1be0cc5f75ed443e3ec944e330da8ccc0e
William Wong <compulim@hotmail.com>
2019-07-25 12:11:27 -0700

    Use NPM version of Speech SDK

--- packages/component/src/SpeechServices/SpeechSDK.js
+++ packages/component/src/SpeechServices/SpeechSDK.js
@@ -1,5 +1,3 @@
-// TODO: [P1] This need to be fixed with a peer depending package, instead of a burnt in bundle.
+import * as CognitiveServicesSpeechSDK from 'microsoft-cognitiveservices-speech-sdk/distrib/es2015/microsoft.cognitiveservices.speech.sdk';
 
-import './external/microsoft.cognitiveservices.speech.sdk.bundle';
-
-export default window.SpeechSDK
+export default CognitiveServicesSpeechSDK
