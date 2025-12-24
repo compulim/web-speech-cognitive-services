faab8ab05f9827c6695acfe4432276bba73a0f10
@compulim <@compulim>
2025-12-24 03:54:01 +0000

    Bump scaffold

---
+++ packages/web-speech-cognitive-services/happy-dom-env.ts
@@ -0,0 +1,7 @@
+import { GlobalRegistrator } from '@happy-dom/global-registrator';
+
+GlobalRegistrator.register({
+  height: 1080,
+  url: 'http://localhost:3000',
+  width: 1920
+});
--- packages/web-speech-cognitive-services/tsup.config.ts
+++ packages/web-speech-cognitive-services/tsup.config.ts
@@ -1,43 +1,13 @@
-import { defineConfig, type Options } from 'tsup';
-
-const baseConfig: Partial<Options> = {
-  define: {
-    'process.env.npm_package_version': `'${process.env.npm_package_version}'`
-  }
-};
+import { defineConfig } from 'tsup';
 
 export default defineConfig([
   {
-    ...baseConfig,
     dts: true,
     entry: {
-      'web-speech-cognitive-services': './src/index.js'
+      'web-speech-cognitive-services': './src/index.ts'
     },
     format: ['cjs', 'esm'],
     sourcemap: true,
-    target: ['chrome100', 'edge100', 'firefox100', 'safari16']
-  },
-  {
-    ...baseConfig,
-    entry: {
-      'web-speech-cognitive-services': './src/index.umd.js'
-    },
-    format: ['iife'],
-    minify: true,
-    outExtension: () => ({ js: '.production.min.js' }),
-    platform: 'browser',
-    sourcemap: true,
-    target: ['chrome100', 'edge100', 'firefox100', 'safari16']
-  },
-  {
-    ...baseConfig,
-    entry: {
-      'web-speech-cognitive-services': './src/index.umd.js'
-    },
-    format: ['iife'],
-    outExtension: () => ({ js: '.development.js' }),
-    platform: 'browser',
-    sourcemap: true,
-    target: ['chrome100', 'edge100', 'firefox100', 'safari16']
+    target: 'esnext'
   }
 ]);
