c89370bf9045cff71fbb6e8bf31511af7f1f5f88
William Wong <compulim@users.noreply.github.com>
2025-12-24 04:00:49 +0000

    Allow JS

---
+++ packages/pages/src/app/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App';
+import createStore from './data/createStore';
+
+const store = createStore();
+
+const [mainElement] = document.getElementsByTagName('main');
+
+mainElement &&
+  createRoot(mainElement)?.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+
+declare global {
+  var IS_DEVELOPMENT: boolean | undefined;
+}
+
+typeof IS_DEVELOPMENT !== 'undefined' &&
+  IS_DEVELOPMENT &&
+  new EventSource('/esbuild').addEventListener('change', () => location.reload());
