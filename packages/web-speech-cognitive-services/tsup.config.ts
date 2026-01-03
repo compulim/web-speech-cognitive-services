c83b00cbc1f5a502c89454a97bf0ffe5fe23537a
William Wong <compulim@users.noreply.github.com>
2026-01-03 04:23:50 +0000

    Revert

--- packages/web-speech-cognitive-services/tsup.config.ts
+++ packages/web-speech-cognitive-services/tsup.config.ts
@@ -1,23 +1,43 @@
 import { defineConfig, type Options } from 'tsup';
-import overrideConfig from './tsup.config.override.ts';
 
-const baseConfig: Options = {
-  dts: true,
-  entry: {
-    'web-speech-cognitive-services': './src/index.ts'
-  },
-  sourcemap: true
+const baseConfig: Partial<Options> = {
+  define: {
+    'process.env.npm_package_version': `'${process.env.npm_package_version}'`
+  }
 };
 
 export default defineConfig([
-  overrideConfig({
+  {
     ...baseConfig,
-    format: ['esm'],
-    target: 'esnext'
-  }),
-  overrideConfig({
+    dts: true,
+    entry: {
+      'web-speech-cognitive-services': './src/index.js'
+    },
+    format: ['cjs', 'esm'],
+    sourcemap: true,
+    target: ['esnext']
+  },
+  {
     ...baseConfig,
-    format: ['cjs'],
-    target: 'es2019' // For compatibility with Webpack 4.
-  })
+    entry: {
+      'web-speech-cognitive-services': './src/index.umd.js'
+    },
+    format: ['iife'],
+    minify: true,
+    outExtension: () => ({ js: '.production.min.js' }),
+    platform: 'browser',
+    sourcemap: true,
+    target: ['chrome100', 'edge100', 'firefox100', 'safari16']
+  },
+  {
+    ...baseConfig,
+    entry: {
+      'web-speech-cognitive-services': './src/index.umd.js'
+    },
+    format: ['iife'],
+    outExtension: () => ({ js: '.development.js' }),
+    platform: 'browser',
+    sourcemap: true,
+    target: ['chrome100', 'edge100', 'firefox100', 'safari16']
+  }
 ]);
